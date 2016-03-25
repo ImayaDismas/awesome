ff21d2b247eab50d9098e53f66b14ee4b13f928a
ImayaDismas <imayadismas@gmail.com>
2016-03-25 12:14:48 +0300

    Added an App activity

---
+++ NewPipe/app/src/main/java/com/dismas/imaya/newpipe/App.java
@@ -0,0 +1,57 @@
+package com.dismas.imaya.newpipe;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by imaya on 3/25/16.
+ */
+public class App extends Application {
+
+    private static boolean useTor;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize image loader
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
+        ImageLoader.getInstance().init(config);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(prefs.getBoolean(getString(R.string.use_tor_key), false)) {
+            OrbotHelper.requestStartTor(this);
+            configureTor(true);
+        } else {
+            configureTor(false);
+        }
+
+        // DO NOT REMOVE THIS FUNCTION!!!
+        // Otherwise downloadPathPreference has invalid value.
+        SettingsActivity.initSettings(this);
+    }
+
+    /**
+     * Set the proxy settings based on whether Tor should be enabled or not.
+     */
+    static void configureTor(boolean enabled) {
+        useTor = enabled;
+        if (useTor) {
+            NetCipher.useTor();
+        } else {
+            NetCipher.setProxy(null);
+        }
+    }
+
+    static void checkStartTor(Context context) {
+        if (useTor) {
+            OrbotHelper.requestStartTor(context);
+        }
+    }
+
+    static boolean isUsingTor() {
+        return useTor;
+    }
+}
